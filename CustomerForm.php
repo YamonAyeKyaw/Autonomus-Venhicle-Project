e939f565031e347a2502a0d1339f2ceb91463d2f
Yamon Aye Kyaw (Nora) <130294254+YamonAyeKyaw@users.noreply.github.com>
2026-07-08 11:08:25 +0630

    Changes in Exposed API key

--- CustomerForm.php
+++ CustomerForm.php
@@ -138,13 +138,13 @@ if(isset($_POST['btnRegister']))
     }
   </style>
   <img src="images/driverless-cars-history-share.jpg" class="imgdchs">
-</div>
+</div>				
 
 
 
     			<!-- <div class="col-md-6">
                     <div class="map-responsive">
-                        <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="600" height="450" frameborder="0" style="border:0; width: 100%;" allowfullscreen></iframe>
+                        <iframe src="https://www.google.com/maps/embed/v1/place?key=getenv('GOOGLE_API_KEY')" width="600" height="450" frameborder="0" style="border:0; width: 100%;" allowfullscreen></iframe>
                     </div>
     			</div> -->
     		</div>
